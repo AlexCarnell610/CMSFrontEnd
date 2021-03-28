e882fe3433b7f86f7d7c10c56778ad699fdeb033
alex <alex.carnell@hotmail.co.uk>
2021-03-28 14:17:44 +0100

    complete footer component testing

---
+++ src/app/components/footer/footer.component.spec.ts
@@ -0,0 +1,13 @@
+import { FooterComponent } from './footer.component';
+
+fdescribe('FooterComponent', () => {
+  let component: FooterComponent;
+
+  beforeEach(() => {
+    component = new FooterComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
