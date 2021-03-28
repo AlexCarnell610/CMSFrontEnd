2a22bfa511bed3ca0bf0743f19b193cefeb12b6d
alex <alex.carnell@hotmail.co.uk>
2021-03-28 13:10:21 +0100

    Loading pane service testing complete

--- libs/services/src/loading-pane.service.spec.ts
+++ libs/services/src/loading-pane.service.spec.ts
@@ -1,1 +1,18 @@
-describe('LoadingPaneService', () => {});
+import { LoadingPaneService } from './loading-pane.service';
+
+fdescribe('LoadingPaneService', () => {
+  let service: LoadingPaneService;
+
+  beforeEach(() => {
+    service = new LoadingPaneService();
+  });
+  it('Should create', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should set the next loading pane state', () => {
+    expect(service.currentLoadingState.value).toBeFalse();
+    service.setLoadingState(true);
+    expect(service.currentLoadingState.value).toBeTrue();
+  });
+});
