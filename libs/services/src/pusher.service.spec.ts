4371fc06c0b0c2cae66e91217996c6508bc9a541
alex <alex.carnell@hotmail.co.uk>
2021-03-28 13:23:05 +0100

    Finish pusher service testing

--- libs/services/src/pusher.service.spec.ts
+++ libs/services/src/pusher.service.spec.ts
@@ -1,1 +1,17 @@
-describe('PusherService', () => {});
+import { PusherService } from './pusher.service';
+
+fdescribe('PusherService', () => {
+  let service: PusherService;
+
+  beforeEach(() => {
+    service = new PusherService();
+  });
+
+  it('should compile', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should be able to bind to the channel', () => {
+    expect(service.channel.bind).toBeTruthy();
+  });
+});
--- libs/services/src/pusher.service.ts
+++ libs/services/src/pusher.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
-import Pusher from 'pusher-js';
+import Pusher, { Channel } from 'pusher-js';
 import { environment } from '../../../src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PusherService {
-  pusher: any;
-  channel: any;
+  private _pusher: Pusher;
+  private _channel: Channel;
 
   constructor() {
-    this.pusher = new Pusher(environment.pusher.key, {
+    this._pusher = new Pusher(environment.pusher.key, {
       cluster: environment.pusher.cluster,
     });
-    this.channel = this.pusher.subscribe('cull-update');
+    this._channel = this._pusher.subscribe('cull-update');
+  }
+
+  get channel(): Channel {
+    return this._channel;
   }
 }
