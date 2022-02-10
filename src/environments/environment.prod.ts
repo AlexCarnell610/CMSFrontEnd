50881d749203edb1983b68c5aa249f2e15298f8c
alex <alex.carnell@hotmail.co.uk>
2022-02-10 22:37:39 +0000

    remove auth config import

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 import { PageURLs } from '@cms-enums';
-import * as authConf from '../../auth_config.json';
 export const environment = {
   production: false,
   auth: {
-    domain: authConf.domain,
-    clientId: authConf.clientId,
+    domain: 'cattle-management-system.eu.auth0.com',
+    clientId: 'HkkCv3GXyrOhjDwK4y7evcP757A22BPS',
     redirectUri: `https://${window.location.host}/CMSFrontEnd/${PageURLs.Login}`,
-    audience: authConf.audience,
+    audience: 'https://cmsBackend.com',
   },
   pusher: {
     key: '2c99d4674e8e6f7e2397',
