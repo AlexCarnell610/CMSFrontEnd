9fbfb7deed8e3bf908db6e8e38fefbbb9adafd07
Alex on Dave <alex.carnell@hotmail.co.uk>
2022-12-01 15:49:32 +0000

    add table for bulk weights

--- src/app/components/main-menu/cattle-components/weight/bulk-weight-modal/bulk-weight-modal.component.ts
+++ src/app/components/main-menu/cattle-components/weight/bulk-weight-modal/bulk-weight-modal.component.ts
@@ -6,12 +6,16 @@ import {
   Validators,
 } from '@angular/forms';
 import { Modals } from '@cms-enums';
-import * as moment from 'moment'
+import { Animal } from '@cms-interfaces';
+import { RootState } from '@cms-ngrx';
+import { selectAnimals } from '@cms-ngrx/animal';
+import { Store } from '@ngrx/store';
+import * as moment from 'moment';
 
 interface IBulkWeight {
-  id: string,
-  weight: string,
-  date: Date
+  id: string;
+  weight: string;
+  date: Date;
 }
 @Component({
   selector: 'cms-bulk-weight-modal',
@@ -22,8 +26,13 @@ export class BulkWeightModalComponent implements OnInit {
   selectedFile: File;
   identifier = Modals.BulkWeightModal;
   bulkWeightForm: FormGroup;
+  correctWeights: IBulkWeight[];
+  notFoundWeights: IBulkWeight[];
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private readonly store: Store<RootState>
+  ) {}
 
   ngOnInit(): void {
     this.bulkWeightForm = this.fb.group({
@@ -34,26 +43,58 @@ export class BulkWeightModalComponent implements OnInit {
   checkFile(): void {
     if (this.selectedFile) {
       let fileReader = new FileReader();
-      fileReader.onload = (e) => {
-        const weightData: string = fileReader.result as string
-        const weightDataArray: string[]  = weightData.split("\r\n")
-        
+      this.store.select(selectAnimals).subscribe((animals) => {
+        fileReader.onload = (e) => {
+          const weightData: string = fileReader.result as string;
+          const weightDataArray: string[] = weightData.split('\r\n');
 
-        console.warn(weightDataArray.shift())
-        console.log(weightDataArray);
-        const mappedWeights = weightDataArray.map(weight => {
-          const currWeight = weight.split(",")
+          console.warn(weightDataArray.shift());
+          console.log(weightDataArray);
+          weightDataArray.pop();
+          const mappedWeights = weightDataArray.map((weight) => {
+            const currWeight = weight.split(',');
 
-          const newWeight: IBulkWeight = {
-            id: currWeight[0],
-            weight: currWeight[1],
-            date: moment(`${currWeight[3]},${currWeight[4]}`, "DD/MM/yyyy,HH:mm:SS").toDate()
+            const newWeight: IBulkWeight = {
+              id: currWeight[0],
+              weight: currWeight[1],
+              date: moment(
+                `${currWeight[3]},${currWeight[4]}`,
+                'DD/MM/yyyy,HH:mm:SS'
+              ).toDate(),
+            };
+            return newWeight;
+          });
+          console.warn(mappedWeights);
+
+          this.correctWeights = mappedWeights.filter(
+            (weight) => this.getAnimalIndex(weight, animals) > -1
+          );
+          
+          this.correctWeights.forEach(
+            (weight) =>
+              (weight.id = animals.find((animal) =>
+                animal.tagNumber.endsWith(weight.id)
+              ).tagNumber)
+          );
+
+          if (this.correctWeights.length < mappedWeights.length) {
+            this.notFoundWeights = mappedWeights.filter(
+              (weight) => this.getAnimalIndex(weight, animals) === -1
+            );
           }
-          return newWeight
-        })
 
-        console.log(mappedWeights);
-      };
+          if (this.notFoundWeights?.length > 0) {
+            console.warn(
+              'Following tag numbers were not found',
+              this.notFoundWeights
+            );
+            console.warn(
+              'Do you still want to add these weights?',
+              this.correctWeights
+            );
+          }
+        };
+      });
       fileReader.readAsText(this.selectedFile);
     }
   }
@@ -72,5 +113,15 @@ export class BulkWeightModalComponent implements OnInit {
 
   get fileInputControl(): AbstractControl {
     return this.bulkWeightForm.get('inputFile');
+  }
+
+  getAnimalIndex(weight: IBulkWeight, animals: Animal[]): number {
+    return animals.findIndex((animal) => animal.tagNumber.endsWith(weight.id));
+  }
+
+  mapToTagAndWeight(weights: IBulkWeight[]): { tag: string; weight: string }[] {
+    return weights.map((weight) => {
+      return { tag: weight.id, weight: weight.weight };
+    });
   }
 }
