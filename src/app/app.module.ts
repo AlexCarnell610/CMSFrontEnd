b1beea58b92d450ed93d261ce6910355b6ecaa84
Alex <alex.carnell@hotmail.co.uk>
2023-10-04 18:51:45 +0100

    improve bulk imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,6 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LoadingModalComponent } from './components/loading-modal/loading-modal.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
-import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
