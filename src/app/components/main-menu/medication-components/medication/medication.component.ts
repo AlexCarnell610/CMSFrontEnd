9845ec91d0cb870fc975c438e8653735dd0f2e8a
Alex <alex.carnell@hotmail.co.uk>
2024-05-05 20:42:16 +0100

    adding video player

--- src/app/components/main-menu/medication-components/medication/medication.component.ts
+++ src/app/components/main-menu/medication-components/medication/medication.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { PageURLs } from '@cms-enums';
 
 @Component({
   selector: 'cms-medication',
   templateUrl: './medication.component.html',
-  styleUrls: ['./medication.component.scss']
+  styleUrls: ['./medication.component.scss'],
 })
 export class MedicationComponent implements OnInit {
+  @ViewChild('video') video: HTMLVideoElement
+  mediaStreamLoaded = false;
+  mediaStream: MediaStream;
+  constructor(private readonly router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  backToMain(): void {
+    this.router.navigate([PageURLs.MainMenu]);
   }
 
+  openCamera(): void {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        this.mediaStreamLoaded = true;
+        this.mediaStream = stream;
+        
+      });
+  }
 }
